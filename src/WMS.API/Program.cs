27b92585379de47b6e1dc4aeb18bf06913a38309
Shakhzod Niyozov <shakhzodniyozov@ya.ru>
2023-11-16 11:17:32 +0500

    Add connection string for postgres

--- src/WMS.API/Program.cs
+++ src/WMS.API/Program.cs
@@ -1,15 +1,16 @@
+using WMS.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+DbInitializer.Init(app.Services);
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
